304da421785ab992e81baef36691a1323589a38a
bsokas <bsokas@seas.upenn.edu>
2019-04-04 21:18:28 -0400

    Add functionality for managing retired ports

--- src/Networking/NetworkState.cpp
+++ src/Networking/NetworkState.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <map>
+#include <vector>
 #include <string>
 #include <sstream>
 
@@ -9,6 +10,7 @@
 class NetworkState {
 	static NetworkState* instance;
 	std::map<unsigned short int, std::string> portsInUse;
+	std::vector<unsigned short int> retiredPorts;
 	unsigned short int startPort;
 
 	//private constructor
@@ -30,20 +32,36 @@ public:
 		unsigned int newPort = startPort++;
 		if (serviceDescriptor.size() == 0) {
 			std::stringstream stream;
-			stream << "NO-ID-" << newPort; 
+			stream << "NO-ID-" << newPort;
 			serviceDescriptor = stream.str();
 		}
 		portsInUse[newPort] = serviceDescriptor;
 		return newPort;
 	};
 
+	unsigned short int checkRetiredPorts(){
+		unsigned short int oldPort = 0;
+		if (retiredPorts.size() > 0) {
+			oldPort = retiredPorts.back();
+			retiredPorts.pop_back();
+		}
+
+		return oldPort;
+	}
+
+	void returnPort(unsigned short int portToClose){
+		if (portsInUse.find(portToClose) == portsInUse.end()) return;
+		portsInUse.erase(portToClose);
+		retiredPorts.push_back(portToClose);
+	}
+
 	void print(){
 		if (portsInUse.size() == 0) printf("No network ports currently in use\n");
 		else {
-			int highest = startPort;
+			int highest = startPort - 1;
 			printf("\tService Descriptor\t\tPort Number\n");
-			while (highest > START_PORT) {
-				printf("\t%s\t\t%d\n", portsInUse[highest].c_str(), highest);
+			while (highest >= START_PORT) {
+				printf("\t%s\t\t\t%d\n", portsInUse[highest].c_str(), highest);
 				highest--;
 			}
 		}
